cadd9f5eb62f497960398dedc128f50b2dc03abf
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-04-23 13:25:32 +0200

    [REFACTOR] Moving accordion logic to separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { accordionData } from "./utils/content";
-import Accordion from "./Accordions/accordion";
+import AccordioLogic from "./Accordions/AccordioLogic";
 
 const App = () => {
   return (
     <div>
-      <div className="accordion">
-        {accordionData.map(({ title, content }) => (
-          <Accordion title={title} content={content} />
-        ))}
-      </div>
+      <AccordioLogic />
     </div>
   );
 };
