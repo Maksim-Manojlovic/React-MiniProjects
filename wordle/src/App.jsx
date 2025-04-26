cdd0f77d10edc40bb7c3c32e452dc678bb2a22a5
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-04-26 13:16:53 +0200

    [ADD] Proxy server and fetching implemented - Proxy server implemented because of cors policy of not letting localhost use api - Fething for random words working

--- wordle/src/App.jsx
+++ wordle/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const API_URL = "/api/api/fe/wordle-words";
 
 const App = () => {
-  return <div></div>;
+  const [solution, setSolution] = useState("");
+  useEffect(() => {
+    const fetchWord = async () => {
+      const response = await fetch(API_URL);
+      const words = await response.json();
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      setSolution(randomWord);
+    };
+    fetchWord();
+  }, []);
+
+  return <div>{solution}</div>;
 };
 
 export default App;
